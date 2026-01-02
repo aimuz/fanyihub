afb9de731e3a0f152936f5e45a5c635e92ef2e5f
aimuz <mr.imuz@gmail.com>
2026-01-02 23:01:13 +0800

    chore: Update linting and formatting scripts, add .prettierignore, and reformat configuration files.

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -1,9 +1,9 @@
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 
 export default {
-    preprocess: vitePreprocess(),
-    compilerOptions: {
-        // Svelte 5 runes mode
-        runes: true,
-    },
+  preprocess: vitePreprocess(),
+  compilerOptions: {
+    // Svelte 5 runes mode
+    runes: true,
+  },
 }
